3e8804651a22c6f138d495274a97f0c56acc2a84
Falco Böhnke <falco.boehnke@blateral.com>
2021-08-26 16:35:42 +0200

    Adapting quicknav slice

--- src/slices/QuickNav.tsx
+++ src/slices/QuickNav.tsx
@@ -1,35 +1,27 @@
 import { QuickNav } from '@blateral/b.kit';
-import {
-    PrismicLink,
-    PrismicKeyText,
-    PrismicSlice,
-    resolveUnknownLink,
-} from 'index';
+
 import React from 'react';
-import { PrismicRelationship } from 'utils/prismic';
+import { ModxSlice } from 'utils/modx';
 
 interface QuickNavItem {
-    link: PrismicLink;
-    label: PrismicKeyText;
+    link: string;
+    label: string;
 }
 
-export interface QuickNavSliceType
-    extends PrismicSlice<'QuickNav', QuickNavItem> {
+export interface QuickNavSliceType extends ModxSlice<'QuickNav', QuickNavItem> {
     primary: {
         active_link?: string;
-        quicknav_data?: PrismicRelationship;
     };
 }
 export const QuickNavSlice: React.FC<QuickNavSliceType> = ({
     primary: { active_link },
     items,
 }) => {
-    console.log('ITEMS', items);
     return (
         <QuickNav
             navItems={items?.map((item) => {
                 return {
-                    link: resolveUnknownLink(item.link) || '',
+                    link: item.link || '',
                     label: item.label || '',
                 };
             })}
