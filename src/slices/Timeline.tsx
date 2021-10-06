e39f3120d186dc16e4a445a82321b97cd13545f2
Lukas Wochner <lukas.wochner@blateral.com>
2021-10-06 14:31:53 +0200

    adding SocialWall slice

--- src/slices/Timeline.tsx
+++ src/slices/Timeline.tsx
@@ -1,6 +1,6 @@
 import { ModxSlice } from '../utils/modx';
 import React from 'react';
-import { Theme, Timeline } from '@blateral/b.kit';
+import { assignTo, Theme, Timeline } from '@blateral/b.kit';
 
 interface TimelineItems {
     label?: string;
@@ -8,16 +8,35 @@ interface TimelineItems {
     text?: string;
 }
 
+type BgMode = 'full' | 'inverted';
+
 export interface TimelineSliceType
     extends ModxSlice<'Timeline', TimelineItems> {
     isActive?: boolean;
+
+    bgMode?: BgMode;
+    bgColor?: string;
 
     theme?: Theme;
 }
 
 export const TimelineSlice: React.FC<TimelineSliceType> = ({
     items,
+    bgMode,
+    bgColor,
     theme,
 }) => {
-    return <Timeline theme={theme} items={items} />;
+    // merging cms and component theme settings
+    const sliceTheme = assignTo(
+        {
+            colors: {
+                mono: {
+                    light: bgColor || '',
+                },
+            },
+        },
+        theme
+    );
+
+    return <Timeline bgMode={bgMode} theme={sliceTheme} items={items} />;
 };
