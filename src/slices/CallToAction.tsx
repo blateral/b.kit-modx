47597ecbd434ccacd7a516f961486b4d882f3792
Falco Böhnke <falco.boehnke@blateral.com>
2021-10-11 13:19:00 +0200

    Merging CTA and Dobule CTA

--- src/slices/CallToAction.tsx
+++ src/slices/CallToAction.tsx
@@ -1,54 +1,47 @@
-import React from 'react';
-import { assignTo, CallToAction, Theme } from '@blateral/b.kit';
-import {
-    BgMode,
-    isExternalLink,
-    isHeadlineTag,
-    isValidAction,
-    ModxImageProps,
-    ModxSlice,
-} from 'utils/modx';
 import { HeadlineTag } from '@blateral/b.kit/lib/components/typography/Heading';
+import React from 'react';
+import { CallToAction } from '@blateral/b.kit';
+import {
+    ModxSlice,
+    BgMode,
+    ModxImageProps,
+    isExternalLink,
+    isValidAction,
+} from 'index';
 
-interface AddressSliceType {
-    icon?: Pick<ModxImageProps, 'small' | 'meta'>;
-    label?: string;
-}
-
-export interface CallToActionSliceType
-    extends ModxSlice<
-        'CallToAction' | 'CallToActionNewsletter',
-        AddressSliceType
-    > {
+export interface CallToActionSliceType extends ModxSlice<'CallToAction'> {
     isActive?: boolean;
-    superTitle?: string;
-    superTitleAs?: HeadlineTag;
-    title?: string;
-    titleAs?: HeadlineTag;
-
-    text?: string;
     bgMode?: BgMode;
-    bgColor?: string;
+    isMirrored?: boolean;
+    mainColumn?: {
+        superTitle?: string;
+        title?: string;
+        superTitleAs?: string;
+        titleAs?: string;
+        text?: string;
+        contact?: {
+            avatar?: Pick<ModxImageProps, 'small' | 'meta'>;
+            description?: string;
+        };
+        badge?: Pick<ModxImageProps, 'small' | 'meta'>;
 
-    contactAvatar?: Pick<ModxImageProps, 'small' | 'meta'>;
-    contact: { name?: string; description?: string };
+        primary_link?: string;
+        primary_label?: string;
+        secondary_link?: string;
+        secondary_label?: string;
+    };
+    secondaryColumn?: {
+        superTitle?: string;
+        title?: string;
+        superTitleAs?: string;
+        titleAs?: string;
+        text?: string;
+        primary_link?: string;
+        primary_label?: string;
+        secondary_link?: string;
+        secondary_label?: string;
+    };
 
-    // FIXME: NEwsletter
-    newsletter_placeholder?: string;
-    newsletter_button_label?: string;
-
-    primary_link?: string;
-    secondary_link?: string;
-    primary_label?: string;
-    secondary_label?: string;
-
-    badge?: Pick<ModxImageProps, 'small' | 'meta'>;
-    // helpers to define component elements outside of slice
-    injectForm?: (props: {
-        isInverted?: boolean;
-        placeholder?: string;
-        buttonLabel?: string;
-    }) => React.ReactNode;
     primaryAction?: (props: {
         isInverted?: boolean;
         label?: string;
@@ -61,126 +54,118 @@ export interface CallToActionSliceType
         href?: string;
         isExternal?: boolean;
     }) => React.ReactNode;
-    theme?: Theme;
 }
 
 export const CallToActionSlice: React.FC<CallToActionSliceType> = ({
-    superTitle,
-    superTitleAs,
-    title,
-    titleAs,
-    text,
     bgMode,
-    bgColor,
-
-    contactAvatar,
-    contact,
-
-    newsletter_placeholder,
-    newsletter_button_label,
-
-    primary_link,
-    primary_label,
-    secondary_link,
-    secondary_label,
-    badge,
-    items,
-    injectForm,
+    mainColumn,
+    secondaryColumn,
     primaryAction,
     secondaryAction,
-    theme,
 }) => {
-    // merging cms and component theme settings
-    const sliceTheme = assignTo(
-        {
-            colors: {
-                mono: {
-                    light: bgColor || '',
-                },
-            },
-        },
-        theme
-    );
-
+    console.log(mainColumn?.contact?.description);
     return (
         <CallToAction
-            theme={sliceTheme}
-            bgMode={bgMode === 'inverted' ? 'inverted' : 'full'}
-            title={title}
-            titleAs={isHeadlineTag(titleAs) ? titleAs : 'div'}
-            superTitle={superTitle}
-            superTitleAs={isHeadlineTag(superTitleAs) ? superTitleAs : 'div'}
-            text={text}
-            contact={
-                contact && contact.name && contactAvatar?.small
-                    ? {
-                          avatar: contactAvatar.small
-                              ? {
-                                    src: contactAvatar?.small || '',
-                                    alt: contactAvatar?.meta?.altText || '',
-                                }
-                              : undefined,
-                          name: contact.name,
-                          description: contact.description,
-                          addresses:
-                              items &&
-                              items?.map((item) => {
-                                  return {
-                                      decorator: item?.icon?.small && (
-                                          <img
-                                              src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}/${item?.icon?.small}`}
-                                              alt={item?.icon.meta?.altText}
-                                          />
-                                      ),
-                                      label: item?.label || '',
-                                  };
-                              }),
-                      }
-                    : undefined
+            superTitleAs={(mainColumn?.superTitleAs as HeadlineTag) || 'div'}
+            titleAs={(mainColumn?.titleAs as HeadlineTag) || 'div'}
+            bgMode={
+                bgMode === 'full' || bgMode === 'inverted' ? bgMode : undefined
             }
-            newsForm={
-                newsletter_placeholder || newsletter_button_label
-                    ? (isInverted?: boolean) =>
-                          injectForm &&
-                          injectForm({
-                              isInverted,
-                              buttonLabel: newsletter_button_label,
-                              placeholder: newsletter_placeholder,
-                          })
-                    : undefined
+            title={mainColumn?.title}
+            superTitle={mainColumn?.superTitle}
+            text={mainColumn?.text}
+            contact={{
+                avatar: {
+                    src: mainColumn?.contact?.avatar?.small || '',
+                    alt: mainColumn?.contact?.avatar?.meta?.altText || '',
+                },
+
+                description: mainColumn?.contact?.description || '',
+            }}
+            badge={
+                mainColumn?.badge ? <img src={mainColumn.badge.small} /> : null
             }
             primaryAction={
-                primaryAction && isValidAction(primary_label, primary_link)
-                    ? (isInverted) =>
+                primaryAction &&
+                isValidAction(
+                    mainColumn?.primary_label,
+                    mainColumn?.primary_link
+                )
+                    ? (isInverted: boolean) =>
                           primaryAction({
                               isInverted,
-                              label: primary_label,
-                              href: primary_link || '',
-                              isExternal: isExternalLink(primary_link),
+                              label: mainColumn?.primary_label,
+                              href: mainColumn?.primary_link || '',
+                              isExternal: isExternalLink(
+                                  mainColumn?.primary_link
+                              ),
                           })
                     : undefined
             }
             secondaryAction={
                 secondaryAction &&
-                isValidAction(secondary_label, secondary_link)
-                    ? (isInverted) =>
+                isValidAction(
+                    mainColumn?.primary_label,
+                    mainColumn?.primary_link
+                )
+                    ? (isInverted: boolean) =>
                           secondaryAction({
                               isInverted,
-                              label: secondary_label,
-                              href: secondary_link || '',
-                              isExternal: isExternalLink(secondary_link),
+                              label: mainColumn?.secondary_label,
+                              href: mainColumn?.secondary_link || '',
+                              isExternal: isExternalLink(
+                                  mainColumn?.secondary_link
+                              ),
                           })
                     : undefined
             }
-            badge={
-                badge &&
-                badge.small && (
-                    <img
-                        src={`${badge.small}`}
-                        alt={badge.meta?.altText || ''}
-                        style={{ height: '100%', width: '100%' }}
-                    />
-                )
+            column={
+                secondaryColumn?.title ||
+                secondaryColumn?.superTitle ||
+                secondaryColumn?.text
+                    ? {
+                          title: secondaryColumn?.title,
+                          superTitle: secondaryColumn?.superTitle,
+                          text: secondaryColumn?.text,
+                          primaryAction:
+                              primaryAction &&
+                              isValidAction(
+                                  secondaryColumn?.primary_label,
+                                  secondaryColumn?.primary_link
+                              )
+                                  ? (isInverted: boolean) =>
+                                        primaryAction({
+                                            isInverted,
+                                            label: secondaryColumn?.primary_label,
+                                            href:
+                                                secondaryColumn?.primary_link ||
+                                                '',
+                                            isExternal: isExternalLink(
+                                                secondaryColumn?.primary_link
+                                            ),
+                                        })
+                                  : undefined,
+
+                          secondaryAction:
+                              secondaryAction &&
+                              isValidAction(
+                                  secondaryColumn?.secondary_label,
+                                  secondaryColumn?.secondary_link
+                              )
+                                  ? (isInverted: boolean) =>
+                                        secondaryAction({
+                                            isInverted,
+                                            label: secondaryColumn?.secondary_label,
+                                            href:
+                                                secondaryColumn?.secondary_link ||
+                                                '',
+                                            isExternal: isExternalLink(
+                                                secondaryColumn?.secondary_link
+                                            ),
+                                        })
+                                  : undefined,
+                      }
+                    : undefined
             }
         />
     );
