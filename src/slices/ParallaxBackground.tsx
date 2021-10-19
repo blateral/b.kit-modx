468e0997612dc4242611c5223d836d68746080db
Falco Böhnke <falco.boehnke@blateral.com>
2021-10-19 09:52:11 +0200

    Adding paralax background settings

--- src/slices/ParallaxBackground.tsx
+++ src/slices/ParallaxBackground.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { ParallaxBackground } from '@blateral/b.kit';
 import { ModxSlice } from '../utils/modx';
 
-export interface ParallaxBackgroundSliceType extends ModxSlice<'ParallaxBackground'> {
+export interface ParallaxBackgroundSliceType
+    extends ModxSlice<'ParallaxBackground'> {
     isActive?: boolean;
     image?: string;
+    hAlign?: 'left' | 'center' | 'right';
+    contentWidth?: number;
 }
 
 export const ParallaxBackgroundSlice: React.FC<ParallaxBackgroundSliceType> = ({
     image,
+    hAlign,
+    contentWidth,
 }) => {
     if (!image) return null;
-    return <ParallaxBackground image={<img src={`${image}`} />} />;
+    return (
+        <ParallaxBackground
+            // In %
+            contentWidth={contentWidth}
+            hAlign={hAlign}
+            image={image ? <img src={`${image}`} /> : false}
+        />
+    );
 };
