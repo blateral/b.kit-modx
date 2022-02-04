fbb1aad27a26b20362df9d88a8945a841e90c633
Falco <falco.boehnke@gmail.com>
2022-02-04 16:58:44 +0100

    Implementing new map props

--- src/slices/Map.tsx
+++ src/slices/Map.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 
 import { assignTo, FlyToIcon, Map, Theme } from '@blateral/b.kit/lib';
-import { isExternalLink, isValidAction, ModxSlice } from 'utils/modx';
+import {
+    isExternalLink,
+    isHeadlineTag,
+    isValidAction,
+    ModxSlice,
+} from 'utils/modx';
+import { MapLocation } from '@blateral/b.kit/lib/components/sections/Map';
+import { HeadlineTag } from '@blateral/b.kit/lib/components/typography/Heading';
 
 interface MapLocationItems {
     marker?: string;
-
     longitude?: string;
     latitude?: string;
+
     title?: string;
     titleAs?: string;
+    superTitle?: string;
+    superTitleAs?: HeadlineTag;
+
+    companyName?: string;
+    address: {
+        street: string;
+        postalCode: string;
+        city?: string;
+        country: string;
+    };
+    //**Deprecated */
     contact?: string;
+
+    telephone?: {
+        link?: string;
+        label?: string;
+        icon?: (props: { isInverted?: boolean }) => React.ReactNode;
+    };
+    email?: {
+        link?: string;
+        label?: string;
+        icon?: (props: { isInverted?: boolean }) => React.ReactNode;
+    };
+    // JSON-LD only prop
+    image?: string[];
+    description?: string;
 
     primary_link?: string;
     primary_label?: string;
@@ -66,9 +98,9 @@ export interface MapSliceType extends ModxSlice<'Map', MapLocationItems> {
         isActive?: boolean;
         index?: number;
     }) => React.ReactNode;
-    phoneIcon?: (isInverted?: boolean) => React.ReactNode;
-    mailIcon?: (isInverted?: boolean) => React.ReactNode;
-    routingIcon?: (isInverted?: boolean) => React.ReactNode;
+    phoneIcon?: (props: { isInverted?: boolean }) => React.ReactNode;
+    mailIcon?: (props: { isInverted?: boolean }) => React.ReactNode;
+    routingIcon?: (props: { isInverted?: boolean }) => React.ReactNode;
     theme?: Theme;
 }
 
@@ -90,7 +122,8 @@ export const MapSlice: React.FC<MapSliceType> = ({
     controlNext,
     controlPrev,
     dot,
-
+    mailIcon,
+    phoneIcon,
     theme,
 }) => {
     // merging cms and component theme settings
@@ -117,13 +150,85 @@ export const MapSlice: React.FC<MapSliceType> = ({
             flyToControl={withFlyTo ? flyToControl || <FlyToIcon /> : undefined}
             allMarkersOnInit={allMarkersOnInit}
             fitBoundsPadding={fitBoundsPadding || [30, 30]}
-            locations={items?.map((location, i) => {
+            locations={items?.map((location, i): MapLocation => {
                 const posLat = location.latitude ? +location.latitude : 0;
                 const posLng = location.longitude ? +location.longitude : 0;
+                console.log('Location: ', location);
 
                 return {
                     id: `location-${i}`,
                     position: [posLat, posLng],
+                    title: location.title || '',
+                    titleAs: isHeadlineTag(location.titleAs)
+                        ? location.titleAs
+                        : 'h2',
+                    superTitle: location.superTitle || '',
+                    superTitleAs: isHeadlineTag(location.superTitleAs)
+                        ? location.superTitleAs
+                        : 'h2',
+                    companyName: location.companyName,
+                    address: location.address,
+                    contact: {
+                        email: {
+                            ...location.email,
+                            icon: mailIcon
+                                ? () =>
+                                      mailIcon({
+                                          isInverted: bgMode === 'inverted',
+                                      })
+                                : undefined,
+                        },
+                        telephone: {
+                            ...location.telephone,
+                            icon: phoneIcon
+                                ? () =>
+                                      phoneIcon({
+                                          isInverted: bgMode === 'inverted',
+                                      })
+                                : undefined,
+                        },
+                    },
+
+                    description: location.description,
+                    image:
+                        location?.image && location.image.length > 0
+                            ? location.image
+                            : undefined,
+
+                    primaryAction:
+                        primaryAction &&
+                        isValidAction(
+                            location.primary_label,
+                            location.primary_link
+                        )
+                            ? ({ isInverted }) =>
+                                  primaryAction({
+                                      isInverted,
+                                      label: location.primary_label,
+                                      href: location.primary_link || '',
+                                      isExternal: isExternalLink(
+                                          location.primary_link
+                                      ),
+                                  })
+                            : undefined,
+                    secondaryAction:
+                        secondaryAction &&
+                        isValidAction(
+                            location.secondary_label,
+                            location.secondary_link
+                        )
+                            ? ({ isInverted }) =>
+                                  secondaryAction({
+                                      isInverted,
+                                      label: location.secondary_label,
+                                      href: location.secondary_link || '',
+                                      isExternal: isExternalLink(
+                                          location.secondary_link
+                                      ),
+                                  })
+                            : undefined,
+
+                    //**DEPRECATED: Only used if new props aren't available */
                     meta: {
                         title: location.title,
                         contact: location.contact,
