5df60602b25b6d9f70be3578c92d68c1b1ac9184
Falco Böhnke <falco.boehnke@blateral.com>
2022-03-31 13:32:51 +0200

    Implementing newnav slicer

--- src/slices/NewNavigation.tsx
+++ src/slices/NewNavigation.tsx
@@ -1,35 +1,60 @@
-import { assignTo, Navigation, ThemeMods } from '@blateral/b.kit';
-import { HeadlineTag } from '@blateral/b.kit/lib/components/typography/Heading';
+import { NewNavigation, ThemeMods } from '@blateral/b.kit';
+import {
+    MenuTypeProps,
+    NavGroup,
+    NavItem,
+} from '@blateral/b.kit/lib/components/sections/navigation/remastered/menu/Menu';
+import {
+    NavBarStates,
+    NavMenuStates,
+} from '@blateral/b.kit/lib/components/sections/navigation/remastered/Navigation';
 import React from 'react';
-import { BgMode, isExternalLink, isValidAction, ModxSlice } from 'utils/modx';
+import { ModxSlice } from 'utils/modx';
 
-export interface NavigationSliceType extends ModxSlice<'Navigation'> {
-    isActive?: boolean;
-    anchor?: {
-        id?: string;
-        label?: string;
+export interface NewNavigationSliceType extends ModxSlice<'Navigation'> {
+    clampWidth?: 'content' | 'full';
+    navbar?: {
+        isStickable?: boolean;
+        isCollapsible?: boolean;
+        pageFlow?: 'overContent' | 'beforeContent';
+        topBg?: string;
+        mainBg?: string;
+        bottomBg?: string;
+        onContentBg?: string;
+        topBar?: (props: NavBarStates) => React.ReactNode;
+        mainBar?: (props: NavBarStates) => React.ReactNode;
+        bottomBar?: (props: NavBarStates) => React.ReactNode;
+        theme?: ThemeMods;
     };
-    bgColor?: string;
-    theme?: ThemeMods;
+
+    menu?: {
+        isIndexPage?: boolean;
+        mainNavigation?: Array<NavGroup>;
+        subNavigation?: Array<NavItem>;
+        header?: (props: NavMenuStates) => React.ReactNode;
+        footer?: (props: NavMenuStates) => React.ReactNode;
+        typeSettings: MenuTypeProps;
+        theme?: ThemeMods;
+    };
 }
 
-export const NavigationSlice: React.FC<NavigationSliceType> = ({
-    anchor,
-    bgColor,
-
-    theme,
+export const NewNavigationSlice: React.FC<NewNavigationSliceType> = ({
+    navbar,
+    menu,
+    clampWidth,
 }) => {
-    // merging cms and component theme settings
-    const sliceTheme = assignTo(
-        {
-            colors: {
-                mono: {
-                    light: bgColor || '',
-                },
-            },
-        },
-        theme
+    return (
+        <NewNavigation clampWidth={clampWidth} navBar={navbar} menu={menu} />
     );
-
-    return <Navigation theme={sliceTheme} />;
 };
+
+// export interface NavItem {
+//     label: string;
+//     link: LinkProps;
+//     isCurrent?: boolean;
+// }
+// export interface NavGroup extends NavItem {
+//     isFeatured?: boolean;
+//     icon?: React.ReactNode;
+//     subItems?: NavItem[];
+// }
