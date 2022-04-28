248a78b878b35bf3615edc7a42ae7595ffa5d4c7
Falco Böhnke <falco.boehnke@blateral.com>
2022-04-28 11:26:58 +0200

    Adding anchor normalization function

--- src/utils/mapping.ts
+++ src/utils/mapping.ts
@@ -1,3 +1,9 @@
 export const isSVG = (url?: string) => {
     return url ? /\.(svg)$/i.test(url) : false;
 };
+
+
+export const normalizeAnchorId = (anchorId?: string) => {
+    return anchorId?.replace(/\s/g, '_').toLowerCase() || '';
+};
+
