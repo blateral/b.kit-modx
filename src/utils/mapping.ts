a5d2fb7af56b01bde6e12b0ad2602a56e50608b3
Falco Böhnke <falco.boehnke@blateral.com>
2022-05-05 18:36:32 +0200

    Adding linkList Html

--- src/utils/mapping.ts
+++ src/utils/mapping.ts
@@ -7,3 +7,21 @@ export const normalizeAnchorId = (anchorId?: string) => {
     return anchorId?.replace(/\s/g, '_').toLowerCase() || '';
 };
 
+
+export const parseLinkListFromHtml = (htmlString: string) => {
+    const paragraphs = Array.from(htmlString.matchAll(/(<p>)(.*?)(<\/p>)/gis));
+    const splittedParagraphs = paragraphs.map((item) => item[0]);
+
+    const classifiedParagraphs = splittedParagraphs.map((paragraph) => {
+        const trimmedParagraph = paragraph.trim().substring(3, 6);
+        if (trimmedParagraph.includes('<a')) {
+            const pair = Array.from(paragraph);
+            pair.splice(2, 0, ' class="linklist"');
+            return pair.join('');
+        } else {
+            return paragraph;
+        }
+    });
+
+    return classifiedParagraphs.join('');
+};
