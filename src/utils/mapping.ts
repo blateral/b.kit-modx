db1744614e2aabf77b0452a663c2e153aa78f01c
Falco Böhnke <falco.boehnke@blateral.com>
2022-05-06 15:01:32 +0200

    Updating richtext parser

--- src/utils/mapping.ts
+++ src/utils/mapping.ts
@@ -10,13 +10,17 @@ export const normalizeAnchorId = (anchorId?: string) => {
 
 export const parseLinkListFromHtml = (htmlString: string) => {
     const paragraphs = Array.from(htmlString.matchAll(/(<p>)(.*?)(<\/p>)/gis));
+    
+    if (paragraphs && paragraphs?.length < 1) return htmlString;
+
     const splittedParagraphs = paragraphs.map((item) => item[0]);
 
+   
     const classifiedParagraphs = splittedParagraphs.map((paragraph) => {
         const trimmedParagraph = paragraph.trim().substring(3, 6);
         if (trimmedParagraph.includes('<a')) {
             const pair = Array.from(paragraph);
-            pair.splice(2, 0, ' class="linklist"');
+            pair.splice(2, 0, ' class="blat-link-list"');
             return pair.join('');
         } else {
             return paragraph;
