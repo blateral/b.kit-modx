1adc9ab1b0fea521137032ec3eefa7a1ac335d28
Lukas Wochner <lukas.wochner@blateral.com>
2022-06-07 16:17:09 +0200

    adding socials mapping function

---
+++ src/utils/mapping.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import { SocialItem } from '@blateral/b.kit/lib/components/blocks/SocialList';
+import { SocialMediaItem } from './modx';
+
+export const isSVG = (url?: string) => {
+    return url ? /\.(svg)$/i.test(url) : false;
+};
+
+export const normalizeAnchorId = (anchorId?: string) => {
+    return anchorId?.replace(/\s/g, '_').toLowerCase() || '';
+};
+
+export const parseLinkListFromHtml = (htmlString: string) => {
+    const paragraphs = Array.from(htmlString.matchAll(/(<p>)(.*?)(<\/p>)/gis));
+
+    if (paragraphs && paragraphs?.length < 1) return htmlString;
+
+    const splittedParagraphs = paragraphs.map((item) => item[0]);
+
+    const classifiedParagraphs = splittedParagraphs.map((paragraph) => {
+        const trimmedParagraph = paragraph.trim().substring(3, 6);
+        if (trimmedParagraph.includes('<a')) {
+            const pair = Array.from(paragraph);
+            pair.splice(2, 0, ' class="blat-link-list"');
+            return pair.join('');
+        } else {
+            return paragraph;
+        }
+    });
+
+    return classifiedParagraphs.join('');
+};
+
+export const mapSocials = (
+    socials?: SocialMediaItem[],
+    isInverted?: boolean
+): SocialItem[] => {
+    if (!socials) return [];
+
+    console.log(socials);
+
+    const mappedSocials = socials
+        .filter((social) => {
+            return social.icon?.small && social.link;
+        })
+        .map<SocialItem>((social) => {
+            const iconUrl = social.icon?.small || '';
+            const invertedIconUrl =
+                social['icon-inverted']?.small || iconUrl || '';
+
+            return {
+                href: social.link || '',
+                icon: ({ title }) => (
+                    <img
+                        alt={title}
+                        src={isInverted ? invertedIconUrl : iconUrl}
+                    />
+                ),
+            };
+        });
+
+    return mappedSocials;
+};
