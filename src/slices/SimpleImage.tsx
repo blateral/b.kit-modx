ad11f2222dffd0b3b0e00617991a06fab25854cf
Lukas Wochner <lukas.wochner@blateral.com>
2022-11-30 15:54:17 +0100

    improving mapping of SimpleImage and bumping beta version

--- src/slices/SimpleImage.tsx
+++ src/slices/SimpleImage.tsx
@@ -8,7 +8,7 @@ export interface SimpleImageSliceType extends ModxSlice<'SimpleImage'> {
     isActive?: boolean;
     anchorId?: string;
     hAlign?: 'left' | 'center' | 'right';
-    image?: ModxImageProps;
+    image?: ModxImageProps & { originals: { w: string; h: string } };
 }
 
 export const SimpleImageSlice: React.FC<SimpleImageSliceType> = ({
@@ -16,11 +16,32 @@ export const SimpleImageSlice: React.FC<SimpleImageSliceType> = ({
     hAlign,
     anchorId,
 }) => {
+    let ratio: { w: number; h: number } | undefined = undefined;
+    if (image?.originals.w && image?.originals.h) {
+        ratio = {
+            w: +image.originals.w,
+            h: +image.originals.h,
+        };
+    }
+
     return (
         <SimpleImage
             hAlign={hAlign}
             anchorId={normalizeAnchorId(anchorId)}
-            image={image?.small ? image : undefined}
+            image={
+                image?.small
+                    ? {
+                          ...image,
+                          alt: image.meta?.altText,
+                          copyright: image.meta?.copyright,
+                          ratios: ratio
+                              ? {
+                                    small: ratio,
+                                }
+                              : undefined,
+                      }
+                    : undefined
+            }
         />
     );
 };
