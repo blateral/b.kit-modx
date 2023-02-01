983c92512a51231aa322a9b72c2d17bc80da4436
Lukas Wochner <lukas.wochner@blateral.com>
2023-02-01 16:31:59 +0100

    updating kit and adding maxThreeCols prop to Cardlist

--- src/slices/CardList.tsx
+++ src/slices/CardList.tsx
@@ -20,6 +20,7 @@ export interface CardListSliceType
     anchorId?: string;
     bgMode?: Omit<BgMode, 'splitted'>;
     bgColor?: string;
+    maxThreeCols?: boolean;
     decorator?: (props: {
         isInverted?: boolean | undefined;
         icon?: string;
@@ -40,6 +41,7 @@ export const CardListSlice: React.FC<CardListSliceType> = ({
     decorator,
     customIcon,
     theme,
+    maxThreeCols,
 }) => {
     // merging cms and component theme settings
     const sliceTheme = assignTo(
@@ -58,6 +60,7 @@ export const CardListSlice: React.FC<CardListSliceType> = ({
             theme={sliceTheme}
             anchorId={normalizeAnchorId(anchorId)}
             bgMode={bgMode as 'full' | 'inverted' | undefined}
+            maxThreeCols={maxThreeCols}
             items={items.map(
                 (item): Omit<CardProps, 'decorator' | 'isInverted'> => {
                     return {
